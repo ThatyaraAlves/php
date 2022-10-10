6567609065a2d02439307d5a12dd6ebe6e32ae65
Thatyara Alves <87332375+ThatyaraAlves@users.noreply.github.com>
2022-10-10 11:37:38 -0300

    Update parte2.php

--- parte2.php
+++ parte2.php
@@ -28,10 +28,27 @@ function writeFille($arquivo){
     return $arquivo;
 }
 
-$
-
-
-
-
-
+writeFille($dados);
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Leitura e Display</title>
+</head>
+<body>
+    
+<?php
+    $file = fopen("texto.txt","r");
+    $teste = fgets($file);
+
+    for()
+    fclose($file);
+
+    echo $teste;
+?>
+</body>
+</html>
